461b930a6b7273a431b7ce500552710eb79ce0bc
Da Huo <wujiuliu@126.com>
2014-10-28 19:36:59 +0800

    update
    add update

--- oldk/server1_1.lua
+++ oldk/server1_1.lua
@@ -1,4 +1,4 @@
--- 增加与客户端数据同步功能 2014.10.27
+-- login with md5 detect 2014.10.27
 
 local skynet = require "skynet"
 local socket = require "socket"
@@ -74,6 +74,9 @@ if mode == "agent" then
 			-- limit request body size to 8192 (you can pass nil to unlimit)
 			local code, url, method, header, body = httpd.read_request(sockethelper.readfunc(id), 8192)
 			print("body:",body)
+			-------------------------------------------------
+			--text analysis
+
 			local temp = {}
 			local fla = string.find(body, "=")
 			local en = string.find(body,"&")
@@ -112,6 +115,7 @@ if mode == "agent" then
 				print("mysql server connected")
 			else
 				print("fuck, it cannot connect to the mysql server")
+				response(id,code,"server error,please try later")
 			end
 
 			if temp["request"] == "register" then
@@ -156,42 +160,53 @@ if mode == "agent" then
 				end
 			end
 
+			if temp["request"]=="update" then
+				local res = db:query("select md5 from cats where username=\'"..temp["username"].."\'")
+				if res[1]["md5"]==temp["md5old"] then
+					res = db:query("update cats set item=\'"..temp["item"].."\' where username=\'"..temp["username"].."\'")
+					res = db:query("update cats set md5=\'"..temp["md5new"].."\' where username=\'"..temp["username"].."\'")
+					response(id,code,"update completed")
+				else
+					response(id,code,"md5 not match,update failed")					
+				end
+			end
 			-- if temp['request']=='checkmd5' then
 				
 			-- end
 
 			-----------------------------------------------
 
-			if code then
-				if code ~= 200 then
-					response(id, code)
-				else
-					local tmp = {}
-					if header.host then
-						table.insert(tmp, string.format("host: %s", header.host))
-					end
-					local path, query = urllib.parse(url)
-					table.insert(tmp, string.format("path: %s", path))
-					if query then
-						local q = urllib.parse_query(query)
-						for k, v in pairs(q) do
-							table.insert(tmp, string.format("query: %s= %s", k,v))
-						end
-					end
-					table.insert(tmp, "-----header----")
-					for k,v in pairs(header) do
-						table.insert(tmp, string.format("%s = %s",k,v))
-					end
-					table.insert(tmp, "-----body----\n" .. body)
-					response(id, code, table.concat(tmp,"\n"))
-				end
-			else
-				if url == sockethelper.socket_error then
-					skynet.error("socket closed")
-				else
-					skynet.error(url)
-				end
-			end
+			-- if code then
+			-- 	if code ~= 200 then
+			-- 		response(id, code)
+			-- 	else
+			-- 		local tmp = {}
+			-- 		if header.host then
+			-- 			table.insert(tmp, string.format("host: %s", header.host))
+			-- 		end
+			-- 		local path, query = urllib.parse(url)
+			-- 		table.insert(tmp, string.format("path: %s", path))
+			-- 		if query then
+			-- 			local q = urllib.parse_query(query)
+			-- 			for k, v in pairs(q) do
+			-- 				table.insert(tmp, string.format("query: %s= %s", k,v))
+			-- 			end
+			-- 		end
+			-- 		table.insert(tmp, "-----header----")
+			-- 		for k,v in pairs(header) do
+			-- 			table.insert(tmp, string.format("%s = %s",k,v))
+			-- 		end
+			-- 		table.insert(tmp, "-----body----\n" .. body)
+			-- 		response(id, code, table.concat(tmp,"\n"))
+			-- 		print("flag")
+			-- 	end
+			-- else
+			-- 	if url == sockethelper.socket_error then
+			-- 		skynet.error("socket closed")
+			-- 	else
+			-- 		skynet.error(url)
+			-- 	end
+			-- end
 			socket.close(id)
 		end)
 	end)
